a580000bff4f300bb92fc8edd498d569cd023106
DiXaT <dimamatveev121@gmail.com>
2022-05-15 22:19:09 +0500

    Buttod add

--- hero-app/pages/index.tsx
+++ hero-app/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Htag } from "../components";
+import { Htag, Button } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h1">Текс</Htag>
+      <Button appearance="primary">Click</Button>
+      <Button appearance="ghost">Click</Button>
+
     </div>
   );
 }
